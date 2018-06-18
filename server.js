9683ff6f9df5c27c24f31c2c334ee5d8ccd01d40
nazarbaran <netrix777@gmail.com>
2018-06-19 00:31:27 +0300

    3 commit

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ var express=require('express');
 var app=express();
 app.use(express.static(__dirname));
 
-var bodyParser=require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+
 
 app.get('/',function(req,res){
 	res.sendFile(__dirname+'/main.html');
